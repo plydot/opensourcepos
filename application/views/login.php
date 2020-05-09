9023291604297eda3e85cfc478569d99bb775319
Whippet Wilson <whippet@plydot.com>
2020-05-09 16:15:49 +0300

    frg

--- application/views/login.php
+++ application/views/login.php
@@ -47,7 +47,7 @@
 			</div>
 		<?php echo form_close(); ?>
 
-		<h1>Open Source Point Of Sale <?php echo $this->config->item('application_version'); ?></h1>
+<!--		<h1>Open Source Point Of Sale --><?php //echo $this->config->item('application_version'); ?><!--</h1>-->
 
 	</div>
 </body>
